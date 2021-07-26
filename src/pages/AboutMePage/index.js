1064067de3129ea8124805d4ee2231cd12022b44
Lee Farnell <leefarnell@mac.com>
2021-07-26 08:48:29 +0100

    Updated AboutMe page

--- src/pages/AboutMePage/index.js
+++ src/pages/AboutMePage/index.js
@@ -1,5 +1,33 @@
+import "./index.css";
+
 const AboutMePage = () => {
-  return <div>About Me</div>;
+  return (
+    <div className="about-me">
+      <div>
+        <p>Hello, and welcome to my portfolio! </p>
+        <p>
+          My name is Lee Farnell and I am currently a student with the
+          University of Birmingham’s Coding Boot Camp. Whilst I’m still in the
+          early stages of the Boot Camp, this portfolio will be an ongoing
+          evolution of my work and the things I have managed to accomplish
+          during my time as a student and beyond. I’ve always had an interest in
+          coding and how web pages are designed and developed to be able to do
+          the things they do.
+        </p>
+        <p>
+          {" "}
+          Thanks to the Bootcamp, I now have solid experience in the following
+          areas: HTML CSS Javascript jQuery Bootstrap jQuery MySQL Git Express
+          Sequelize moment.js
+        </p>
+      </div>
+      <img
+        src="https://leefarnell.github.io/MyPortfolio/assets/images/Me.png"
+        alt="Lee Farnell"
+        className="about-me-image"
+      />
+    </div>
+  );
 };
 
 export default AboutMePage;
