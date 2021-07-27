c5174809b683ad5f937c1595bc0e4eaf6ee007da
Lee Farnell <leefarnell@mac.com>
2021-07-27 11:52:21 +0100

    Added data to get info from GitHub API

--- src/pages/PortfolioPage/index.js
+++ src/pages/PortfolioPage/index.js
@@ -1,9 +1,19 @@
 import "./index.css";
 
+import fetchData from "../../utils/fetchData";
+import Project from "../../components/Project";
+
+const getGithubData = async () => {
+  const data = await fetchData(`https://api.github.com/users/leefarnell/repos`);
+  console.log(data);
+};
+
+getGithubData();
+
 const PortfolioPage = () => {
   return (
     <div>
-      <h1>Hello! 👋</h1>
+      <h1></h1>
     </div>
   );
 };
