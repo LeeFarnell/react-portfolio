488b9c317e841c0c7c116bebbe45bf03c5902fcd
Lee Farnell <leefarnell@mac.com>
2021-07-27 14:10:30 +0100

    Updated Project Page

--- src/pages/PortfolioPage/index.js
+++ src/pages/PortfolioPage/index.js
@@ -3,17 +3,20 @@ import "./index.css";
 import fetchData from "../../utils/fetchData";
 import Project from "../../components/Project";
 
-const getGithubData = async () => {
-  const data = await fetchData(`https://api.github.com/users/leefarnell/repos`);
-  console.log(data);
-};
-
-getGithubData();
-
 const PortfolioPage = () => {
+  const getGithubData = async () => {
+    const data = await fetchData(
+      `https://api.github.com/users/leefarnell/repos`
+    );
+    console.log(data);
+  };
+
+  getGithubData();
+
   return (
-    <div>
-      <h1></h1>
+    <div className="projects">
+      <Project name="My Project" />
+      <Project name="My Project 2" />
     </div>
   );
 };
